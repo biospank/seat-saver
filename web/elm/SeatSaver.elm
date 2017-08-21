990806f9f4dc8f83e0b07c69165031c870cff1dc
biospank <fabio.petrucci@gmail.com>
2017-08-21 18:03:06 +0200

    implement update function

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -1,8 +1,89 @@
 module SeatSaver exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
 
 
-main : Html.Html msg
+-- MODEL
+
+
+type alias Model =
+    List Seat
+
+
+type alias Seat =
+    { seatNo : Int
+    , occupied : Bool
+    }
+
+
+initModel : List Seat
+initModel =
+    [ { seatNo = 1, occupied = False }
+    , { seatNo = 2, occupied = False }
+    , { seatNo = 3, occupied = False }
+    , { seatNo = 4, occupied = False }
+    , { seatNo = 5, occupied = False }
+    , { seatNo = 6, occupied = False }
+    , { seatNo = 7, occupied = False }
+    , { seatNo = 8, occupied = False }
+    , { seatNo = 9, occupied = False }
+    , { seatNo = 10, occupied = False }
+    , { seatNo = 11, occupied = False }
+    , { seatNo = 12, occupied = False }
+    ]
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Toggle Seat
+
+
+update : Msg -> Model -> Model
+update msg seats =
+    case msg of
+        Toggle selectedSeat ->
+            Debug.log "state: "
+                (updateSeat seats selectedSeat)
+
+
+updateSeat : Model -> Seat -> Model
+updateSeat seats selectedSeat =
+    List.map
+        (\seat ->
+            if seat.seatNo == selectedSeat.seatNo then
+                { seat | occupied = not seat.occupied }
+            else
+                seat
+        )
+        seats
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    ul [ class "seats" ] (List.map seatItem model)
+
+
+seatItem : Seat -> Html Msg
+seatItem seat =
+    if seat.occupied then
+        li [ class "seat occupied", onClick (Toggle seat) ] [ text (toString seat.seatNo) ]
+    else
+        li [ class "seat available", onClick (Toggle seat) ] [ text (toString seat.seatNo) ]
+
+
+main : Program Never Model Msg
 main =
-    Html.text "Hello from Elm"
+    beginnerProgram
+        { model = initModel
+        , view = view
+        , update = update
+        }
